ada08f8c252b95a8720e79c96b1793c4d197d6b4
Gene <36591075+gxnx@users.noreply.github.com>
2023-02-16 17:39:14 +0800

    Added comments

--- Program.cs
+++ Program.cs
@@ -6,12 +6,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi); // Learn more about HTTP vs REST API on at https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-vs-rest.html
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()) // Lambda > Function > <YOUR-LAMBDA-FUNCTION-NAME> > Configuration > Environment Variables then Add: ASPNETCORE_ENVIRONMENT = Development
 {
     app.UseSwagger();
     app.UseSwaggerUI();
