fe7e511af935080ca8340785fed7d363d0c94da8
Gene <36591075+gxnx@users.noreply.github.com>
2023-02-20 09:58:11 +0800

    Fixed endpoints for lambda

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace TestNetWebApiOnLambda.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
